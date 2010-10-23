ce60c4ad0eeea22dd0ab4469265230a6c5eec26b
yehgdotnet <git@yehg.net>
2010-10-23 18:07:24 -0400

    updated OS.rb

--- new-plugins/OS.rb
+++ new-plugins/OS.rb
@@ -8,7 +8,7 @@
 Plugin.define "OS" do
 author "Aung Khant, http://yehg.net"
 version "0.1"
-description "Identify OS in server header"
+description "Identify OS in server header and others"
 
 examples=%w|
 http://imagehotel.net
@@ -26,6 +26,17 @@ http://www.microsoft.com
 http://www.asp.net
 http://www.apple.com
 http://ww.test.com
+http://216.200.40.179
+http://216.83.191.232
+http://12.237.96.129
+http://64.71.182.100
+http://64.71.181.79
+http://12.237.96.130
+http://219.166.162.171
+http://89.137.17.241
+http://62.4.73.165
+http://62.92.43.234
+http://89.137.17.241
 |	
 
 def passive
@@ -43,6 +54,16 @@ def passive
 	xpoweredby = @meta['x-powered-by'] if @meta.keys.include?("x-powered-by")
 	if xpoweredby =~ /\asp\.net/
 		m << {:string=>'Windows'}	
+	end
+	# http://www.shodanhq.com/?q=IBM+Corporation
+	# Servlet-Engine: Tomcat Web Server/3.2.4 (JSP 1.1; Servlet 2.2; Java 1.3.0; Linux 2.4.2-SGI_XFS_1.0smp x86; java.vendor=IBM Corporation) 
+	servleteng = @meta['servlet-engine'] if @meta.keys.include?("servlet-engine")
+	if servleteng =~ /\((.*?); (.*?); (.*?); (.*?); java.vendor=IBM Corporation\)/
+		jsp= $1;servlet=$2;java= $3; os=$4;
+		m << {:string=>jsp.to_a}
+		m << {:string=>servlet.to_a}
+		m << {:string=>java.to_a}	
+		m << {:string=>os.to_a}		
 	end
 	m
 end
